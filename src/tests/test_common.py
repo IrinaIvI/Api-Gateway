ca721db6825ee78626fb648ad8f4d4999b15c4a6
Irina Ivashko <ira.ivashko.99@gmail.com>
2024-08-19 21:21:21 +0000

    Рефакторинг тестов

--- src/tests/test_common.py
+++ src/tests/test_common.py
@@ -95,15 +95,15 @@ async def test_get_transaction(user_id, token, start, end, mocker: MockerFixture
     assert response == {'transactions': []}
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize('token', [
-    pytest.param(test_token, id='is correct'),
-    pytest.param('invalid_token', id='is not correct', marks=pytest.mark.xfail())
+@pytest.mark.parametrize('user_id, token', [
+    pytest.param(1, test_token, id='is correct'),
+    pytest.param(1, 'invalid_token', id='is not correct', marks=pytest.mark.xfail())
 ])
-async def test_validate(token, mocker: MockerFixture):
+async def test_validate(user_id, token, mocker: MockerFixture):
     mock_response = mocker.Mock(spec=httpx.Response)
     mock_response.status_code = 200
     mocker.patch('app.common.handle_request', return_value=mock_response)
-    response = await api_validate(token)
+    response = await api_validate(user_id, token)
     assert response is True
 
 
